dd7bc4ba6316062c5bbb86e970f31c43a6558d85
hyun1moon <hyunil.moon@gmail.com>
2023-11-23 17:24:02 +0900

    update 3-1-4

--- chapter_11/step_3_1_4.py
+++ chapter_11/step_3_1_4.py
@@ -12,13 +12,14 @@ from docx.shared import Mm, Pt, RGBColor
 
 import step_0
 import step_1_2
+import step_1_3
 
 #######################################
 # 2. 환경설정
 #######################################
 STEP_3_1 = step_0.OUTPUT_FOLDER / "step_3_1.docx"
 PAGE_WIDTH, PAGE_HEIGHT, PAGE_MARGIN = Mm(210), Mm(297), Mm(12.7)
-GRAPH_WIDTH, GRAPH_HEIGHT = Mm(30), Mm(8)
+GRAPH_WIDTH, GRAPH_HEIGHT, CELL_WIDTH = Mm(30), Mm(8), Mm(35.5)
 
 
 #######################################
@@ -78,6 +79,60 @@ def main():
 
     p_blank = document.add_paragraph()
     p_blank.add_run(" ").font.size = Pt(10)
+
+    table = document.add_table(rows=1, cols=5)
+    table.alignment = WD_TABLE_ALIGNMENT.CENTER
+    table.allow_autofit = False
+    row_1 = table.rows[0]
+
+    with pd.ExcelFile(step_1_2.STEP_1_2) as xlsx:
+        sheet_names = [x for x in xlsx.sheet_names if x != "기준금리M"]
+        for idx, name in enumerate(sheet_names):
+            print(idx, name)
+            df_raw = pd.read_excel(xlsx, sheet_name=name, index_col="TIME")
+            s_value = df_raw["DATA_VALUE"]
+            s_diff = s_value.diff()
+            s_pct_change = s_value.pct_change()
+
+            last_value = s_value.iloc[-1]
+            last_diff = s_diff.iloc[-1]
+            last_diff_rate = s_pct_change.iloc[-1]
+            rgb = "FF0000" if last_diff > 0 else "0000FF" if last_diff < 0 else "000000"
+            last_dt = s_value.index[-1].to_pydatetime()
+            img_path = step_1_3.STEP_1_3.as_posix().format(name)
+
+            arrow = "▲" if last_diff > 0 else "▼" if last_diff < 0 else ""
+            change = f"{arrow}{last_diff:,.2f}  {last_diff_rate:+,.2%}"
+
+            cell = row_1.cells[idx]
+            cell.width = CELL_WIDTH
+            [cell.add_paragraph() for _ in range(4)]
+            p1, p2, p3, p4, p5 = cell.paragraphs
+
+            p1_run = p1.add_run(name)
+            p1_run.font.size = Pt(12)
+            p1_run.font.bold = True
+            p1_run.font.color.rgb = RGBColor.from_string("333333")
+
+            p2_run = p2.add_run(f"{last_value:,.2f}")
+            p2_run.font.size = Pt(14)
+            p2_run.font.bold = True
+            p2_run.font.color.rgb = RGBColor.from_string("333333")
+
+            p3_run = p3.add_run(change)
+            p3_run.font.size = Pt(10)
+            p3_run.font.bold = True
+            p3_run.font.color.rgb = RGBColor.from_string(rgb)
+
+            p4.add_run().add_picture(img_path, width=GRAPH_WIDTH, height=GRAPH_HEIGHT)
+            p4.paragraph_format.space_after = Mm(1)
+            p4.paragraph_format.space_before = Mm(1)
+            p4.paragraph_format.left_indent = Mm(-1)
+
+            p5_run = p5.add_run(last_dt.strftime("%Y-%m-%d"))
+            p5_run.font.size = Pt(8)
+            p5_run.font.bold = True
+            p5_run.font.color.rgb = RGBColor.from_string("888888")
 
     document.save(STEP_3_1)
 
