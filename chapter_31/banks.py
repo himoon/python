79f2be489f089bdc9e11ccc84bfc5a21c01f0d3a
hyun1moon <hyunil.moon@gmail.com>
2023-12-15 09:52:37 +0900

    Update banks

--- chapter_31/banks.py
+++ chapter_31/banks.py
@@ -2,175 +2,119 @@ from __future__ import annotations
 
 import logging
 import pathlib
-import re
-import time
-from pprint import pprint
 
 import pandas as pd
-import requests
 from datakart import Jusogokr, Kakao, Naver
 from tqdm import tqdm
 
-logger = logging.getLogger(__name__)
-
-
-class Sgis:
-    """통계지리정보서비스 SGIS"""
-
-    def __init__(
-        self,
-        api_key: str,
-        api_sec: str,
-    ) -> None:
-        self.api_key: str = api_key
-        self.api_sec: str = api_sec
-
-    @staticmethod
-    def raise_for_err_cd(parsed: dict) -> None:
-        err_cd = parsed.get("errCd", 0)
-        if err_cd:
-            raise ValueError(f"[{err_cd}] {parsed.get('errMsg', 0)}")
-
-    @property
-    def access_token(self) -> str:
-        if not hasattr(self, "_token") or int(self._timeout) / 1000 - 10 < time.time():
-            self.auth()
-        return self._token
-
-    def auth(self) -> dict:
-        # https://sgis.kostat.go.kr/developer/html/newOpenApi/api/dataApi/basics.html#auth
-        url = "https://sgisapi.kostat.go.kr/OpenAPI3/auth/authentication.json"
-        params = dict(consumer_key=self.api_key, consumer_secret=self.api_sec)
-        resp = requests.get(url, params=params)
-        parsed = resp.json()
-        self.raise_for_err_cd(parsed)
-
-        result = parsed.get("result", {})
-        self._timeout = result.get("accessTimeout", 0)
-        self._token = result.get("accessToken", "")
-        return result
-
-    def geocode(
-        self,
-        address: str,
-        page: int = 0,
-        limit: int = 5,
-    ) -> dict:
-        # https://sgis.kostat.go.kr/developer/html/newOpenApi/api/dataApi/addressBoundary.html#geocode
-        url = "https://sgisapi.kostat.go.kr/OpenAPI3/addr/geocodewgs84.json"
-        params = dict(
-            accessToken=self.access_token,
-            address=f"{address}",
-            pagenum=f"{page}",
-            resultcount=f"{limit}",
-        )
-        resp = requests.get(url, params=params)
-        parsed = resp.json()
-        self.raise_for_err_cd(parsed)
-
-        result = parsed.get("result", {})
-        total = result.get("totalcount")
-
-        return parsed
-
-
 WORK_DIR = pathlib.Path(__file__).parent
-INPUT = WORK_DIR / "banks.xlsx"
-OUTPUT = WORK_DIR / "banks_output.xlsx"
+OUTPUT_DIR = WORK_DIR / "output"
+IN_EXCEL = WORK_DIR / "banks.xlsx"
+OUT_EXCEL = OUTPUT_DIR / "banks_output.xlsx"
 
 NAVER_KEY = "4LmEBVOylhQ9Da_Rhryr"
 NAVER_SEC = "WwF0cMKQKS"
 JUSO_KEY = "devU01TX0FVVEgyMDIzMTIxNDAwMzkyNjExNDM1NDg="
 KAKAO_KEY = "2f15ef773b35f21f74877b7ed5122a76"
 
-df_raw = pd.read_excel(INPUT, sheet_name="반기보('23.6월말)", skiprows=3, header=None)
-df_raw.head(5)
 
-df_header = df_raw.iloc[0:2].ffill(axis="rows").ffill(axis="columns")
-ser_header = df_header.apply(lambda x: ".".join(x) if len(x.unique()) > 1 else x[0], axis="rows")
-ser_header
-
-df_reset = df_raw.iloc[2:].reset_index(drop=True)
-# df_sliced.columns = ser_header
-df_reset.columns = ["은행명", "점포명", "점포구분", "시도", "시군구", "읍면동", "도로명", "전화번호"]
-df_reset.fillna("", inplace=True)
-df_reset.head()
-
-df_queried = df_reset.query("점포구분=='지점' and 은행명=='산업은행'").sort_values(["은행명", "점포명"]).reset_index(drop=True)
-df_filtered = df_queried.filter(["은행명", "점포명", "점포구분", "시도", "시군구", "도로명"])
-df_filtered.head()
-
-df_validated = df_filtered.copy()
-df_validated = df_validated.fillna("").applymap(lambda x: str(x).strip())
-df_validated["도로명"] = df_validated["도로명"].replace(r"\(.+\)", "", regex=True)
-# df_validated.apply(lambda x: print(x.unique()), axis=0)
-df_validated.to_excel(OUTPUT)
-
-df_validated["은행명"].sort_values()
-df_validated["점포명"].sort_values()
-df_validated["점포구분"].sort_values()
-df_validated["시도"].sort_values()
-df_validated["시군구"].sort_values()
-df_validated["도로명"].sort_values()
-
-juso = Jusogokr(JUSO_KEY)
-naver = Naver(NAVER_KEY, NAVER_SEC)
-kakao = Kakao(KAKAO_KEY)
+logger = logging.getLogger(__name__)
 
 
-for idx, ser in tqdm(df_validated.iterrows(), total=df_validated.shape[0]):
-    # ser = df_validated.loc[884]
-    addr = f'{ser["시도"]} {ser["시군구"]} {ser["도로명"]}'
-    if not addr.strip():
-        df_validated.loc[idx, "siNm"] = ""
-        df_validated.loc[idx, "sggNm"] = ""
-        df_validated.loc[idx, "roadNm"] = ""
-        continue
+def init_output_folder():
+    if not OUTPUT_DIR.is_dir():
+        print(f"'{OUTPUT_DIR}' 폴더를 생성합니다.")
+        OUTPUT_DIR.mkdir()
 
-    rows = juso.addr(addr)
-    if not rows:
-        logger.error(f"{addr} not found, try kakao")
+
+def load_excel() -> pd.DataFrame:
+    df_raw = pd.read_excel(IN_EXCEL, sheet_name="반기보('23.6월말)", skiprows=3, header=None)
+    df_body = df_raw.iloc[2:].reset_index(drop=True)
+    df_body.columns = ["은행명", "점포명", "점포구분", "시도", "시군구", "읍면동", "도로명", "전화번호"]
+    # df_header = df_raw.iloc[0:2].ffill(axis="rows").ffill(axis="columns")
+    # ser_header = df_header.apply(lambda x: ".".join(x) if len(x.unique()) > 1 else x[0], axis="rows")
+    # df_body.columns = ser_header
+    return df_body.fillna("").map(lambda x: str(x).strip())
+
+
+def main():
+    df_raw = load_excel()
+    # df_query = df_raw.query("점포구분=='지점' and 은행명=='산업은행'").sort_values(["은행명", "점포명"]).reset_index(drop=True)
+    df_query = df_raw.query("점포구분=='지점'").sort_values(["은행명", "점포명"]).reset_index(drop=True)
+    df_filter = df_query.filter(["은행명", "점포명", "점포구분", "시도", "시군구", "도로명"])
+    df_filter.head()
+
+    df_filter["도로명"] = df_filter["도로명"].replace(r"\(.+\)", "", regex=True)  # cleansing
+    df_filter.to_excel(OUT_EXCEL)
+
+    # df_validated.apply(lambda x: print(x.unique()), axis=0)
+    df_filter["은행명"].sort_values()
+    df_filter["점포명"].sort_values()
+    df_filter["점포구분"].sort_values()
+    df_filter["시도"].sort_values()
+    df_filter["시군구"].sort_values()
+    df_filter["도로명"].sort_values()
+
+    juso = Jusogokr(JUSO_KEY)
+    naver = Naver(NAVER_KEY, NAVER_SEC)
+    kakao = Kakao(KAKAO_KEY)
+    # kakao.local_keyword("국민은행 양재PB센터")
+
+    for idx, ser in tqdm(df_filter.iterrows(), total=df_filter.shape[0]):
+        # ser = df_validated.loc[884]
         br_name = f'{ser["은행명"]} {ser["점포명"]} {ser["점포구분"]}'
-        kakao_resp = kakao.local_keyword(br_name)
-        if kakao_resp:
-            kakao_row = kakao_resp[0]
-            addr = kakao_row["road_address_name"]
-            rows = juso.addr(addr)
+        addr = f'{ser["시도"]} {ser["시군구"]} {ser["도로명"]}'
+        if not addr.strip():
+            df_filter.loc[idx, "siNm"] = ""
+            df_filter.loc[idx, "sggNm"] = ""
+            df_filter.loc[idx, "roadNm"] = ""
+            continue
 
-    if not rows:
-        logger.error(f"{addr} not found, try naver")
-        naver_resp = naver.local(br_name)
-        if naver_resp:
-            naver_row = naver_resp[0]
-            addr = naver_row["roadAddress"]
-            rows = juso.addr(addr)
+        rows = juso.addr(addr)
+        if not rows:
+            logger.warning(f"* {idx=}, {br_name=}, {addr=}, not found, try kakao")
+            kakao_resp = kakao.local_keyword(br_name)
+            if kakao_resp:
+                kakao_row = kakao_resp[0]
+                addr = kakao_row.get("road_address_name", "")
+                rows = juso.addr(addr)
 
-    if not rows:
-        logger.error(f"{addr} not found")
-        df_validated.loc[idx, "siNm"] = ""
-        df_validated.loc[idx, "sggNm"] = ""
-        df_validated.loc[idx, "roadNm"] = ""
-        continue
+        if not rows:
+            logger.warning(f"* {idx=}, {br_name=}, {addr=}, not found, try naver")
+            naver_resp = naver.local(br_name)
+            if naver_resp:
+                naver_row = naver_resp[0]
+                addr = naver_row.get("roadAddress", "")
+                rows = juso.addr(addr)
 
-    row = rows[0]
-    df_validated.loc[idx, "siNm"] = row.get("siNm", "")
-    df_validated.loc[idx, "sggNm"] = row.get("sggNm", "")
+        if not rows:
+            logger.error(f"* {idx=}, {br_name=}, {addr=}, not found, skip this row")
+            df_filter.loc[idx, "siNm"] = ""
+            df_filter.loc[idx, "sggNm"] = ""
+            df_filter.loc[idx, "roadNm"] = ""
+            continue
 
-    road_nm = row.get("rn", "")
-    buld_main = row.get("buldMnnm", "")
-    buld_sub = row.get("buldSlno", "")
-    buld_nm = buld_main if buld_sub == "0" else f"{buld_main}-{buld_sub}"
-    df_validated.loc[idx, "roadNm"] = f"{road_nm} {buld_nm}"
+        row = rows[0]
+        df_filter.loc[idx, "siNm"] = row.get("siNm", "")
+        df_filter.loc[idx, "sggNm"] = row.get("sggNm", "")
+
+        road_nm = row.get("rn", "")
+        buld_main = row.get("buldMnnm", "")
+        buld_sub = row.get("buldSlno", "")
+        buld_nm = buld_main if buld_sub == "0" else f"{buld_main}-{buld_sub}"
+        df_filter.loc[idx, "roadNm"] = f"{road_nm} {buld_nm}"
+
+    same_sido = df_filter["시도"] == df_filter["siNm"]
+    same_sgg = df_filter["시군구"] == df_filter["sggNm"]
+    same_roadnm = df_filter["도로명"] == df_filter["roadNm"]
+    same_addr = same_sido & same_sgg & same_roadnm
+
+    df_filter["updated"] = True
+    df_filter.loc[same_addr, "updated"] = False
+    df_filter.to_excel(OUT_EXCEL, index=False)
 
 
-same_sido = df_validated["시도"] == df_validated["siNm"]
-same_sgg = df_validated["시군구"] == df_validated["sggNm"]
-same_roadnm = df_validated["도로명"] == df_validated["roadNm"]
-same_addr = same_sido & same_sgg & same_roadnm
-
-df_validated["original"] = False
-df_validated.loc[same_addr, "original"] = True
-
-df_validated.to_excel(OUTPUT, index=False)
-
-# "서울특별시 강남구 언주로 706 우정빌딩 not found", 1695
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, filename=OUTPUT_DIR / "err.log")
+    init_output_folder()
+    main()
